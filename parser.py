ad6c15f40ad7cb939a21d41ac2a59b34c2d2cbde
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-05-22 22:11:23 +0200

    parser: changed information flow

--- parser.py
+++ parser.py
@@ -1,20 +1,32 @@
 #!/bin/python
 
 import argparse
+from pathlib import Path
+from input_control import InputControl, InputControlFailed
 
 class FileParser:
-    __args : argparse.Namespace
+    __infile : Path
+    __outfile : Path
 
     def __init__(self):
-        self.__args = argparse.Namespace()
+        self.__infile = Path()
+        self.__outfile = Path()
 
     @property
-    def args(self):
-        return self.__args
+    def infile(self):
+        return self.__infile
 
-    @args.setter
-    def args(self, args):
-        self.__args = args
+    @infile.setter
+    def infile(self, infile):
+        self.__infile = infile
+
+    @property
+    def outfile(self):
+        return self.__outfile
+
+    @outfile.setter
+    def outfile(self, outfile):
+        self.__outfile = outfile
 
     def parse_args(self):
         parser = argparse.ArgumentParser(description='XML, JSON, YAML converter')
@@ -22,10 +34,21 @@ class FileParser:
         parser.add_argument('infile', nargs=1, help='input file')
         parser.add_argument('outfile', nargs=1, help='converted file')
 
-        self.args = parser.parse_args()
+        args = parser.parse_args()
+        self.input_control(args.infile[0], args.outfile[0])
+
+    def input_control(self, inarg, outarg):
+        try:
+            # try to validate the input
+            ctrl = InputControl(inarg, outarg)
+
+            # if successful, assign to class attributes
+            self.infile = ctrl.infile
+            self.outfile = ctrl.outfile
+
+        except InputControlFailed as e:
+            print(e)
 
 if __name__ == '__main__':
     parser = FileParser()
     parser.parse_args()
-    print(parser.args)
-
