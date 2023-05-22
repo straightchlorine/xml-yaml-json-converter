319a8d6f06971afcab7635075d786d8ec2681b7a
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-05-22 20:37:10 +0200

    parser: turned the functions into class

--- parser.py
+++ parser.py
@@ -2,14 +2,30 @@
 
 import argparse
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='XML, JSON, YAML converter')
+class FileParser:
+    __args : argparse.Namespace
 
-    parser.add_argument('infile', nargs=1, help='input file')
-    parser.add_argument('outfile', nargs=1, help='converted file')
+    def __init__(self):
+        self.__args = argparse.Namespace()
 
-    files = parser.parse_args()
+    @property
+    def args(self):
+        return self.__args
+
+    @args.setter
+    def args(self, args):
+        self.__args = args
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description='XML, JSON, YAML converter')
+
+        parser.add_argument('infile', nargs=1, help='input file')
+        parser.add_argument('outfile', nargs=1, help='converted file')
+
+        self.args = parser.parse_args()
 
 if __name__ == '__main__':
-     parse_args()
+    parser = FileParser()
+    parser.parse_args()
+    print(parser.args)
 
